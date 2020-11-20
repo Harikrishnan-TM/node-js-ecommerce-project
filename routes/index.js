5c02d58bcd6e77e07678da5cd709751b7edacfea
Harikrishnan-TM <Techdatamaxonline@gmail.com>
2020-11-20 11:21:35 -0800

    creating loop and passing the products in index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,36 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  let products=[
+    {
+      name:"apple",
+      category:'mobile',
+      description:"good one",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQwYNLRJHnP4Y7cG9gOXbEYKkLtfVDuVzh8Yg&usqp=CAU"
+    },
+    {
+      name:"apple",
+      category:'mobile',
+      description:"good one",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQwYNLRJHnP4Y7cG9gOXbEYKkLtfVDuVzh8Yg&usqp=CAU"
+    },
+    {
+      name:"apple",
+      category:'mobile',
+      description:"good one",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQwYNLRJHnP4Y7cG9gOXbEYKkLtfVDuVzh8Yg&usqp=CAU"
+    },
+    {
+      name:"apple",
+      category:'mobile',
+      description:"good one",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQwYNLRJHnP4Y7cG9gOXbEYKkLtfVDuVzh8Yg&usqp=CAU"
+    }
+  ]
+
+
+  res.render('index', { products });
 });
 
 module.exports = router;
