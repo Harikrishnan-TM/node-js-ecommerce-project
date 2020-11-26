18e1363e6400bb9ccf9bf5088767fe38a7a226ef
Harikrishnan-TM <Techdatamaxonline@gmail.com>
2020-11-25 21:50:01 -0800

    passing dummy data

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,33 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('admin/view-products',{admin:true})
+  let products=[
+    {
+      name:"apple",
+      category:'mobile',
+      description:"good one",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQwYNLRJHnP4Y7cG9gOXbEYKkLtfVDuVzh8Yg&usqp=CAU"
+    },
+    {
+      name:"apple",
+      category:'mobile',
+      description:"good one",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQwYNLRJHnP4Y7cG9gOXbEYKkLtfVDuVzh8Yg&usqp=CAU"
+    },
+    {
+      name:"apple",
+      category:'mobile',
+      description:"good one",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQwYNLRJHnP4Y7cG9gOXbEYKkLtfVDuVzh8Yg&usqp=CAU"
+    },
+    {
+      name:"apple",
+      category:'mobile',
+      description:"good one",
+      image:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQwYNLRJHnP4Y7cG9gOXbEYKkLtfVDuVzh8Yg&usqp=CAU"
+    }
+  ]
+  res.render('admin/view-products',{admin:true,products})
 });
 
 module.exports = router;
