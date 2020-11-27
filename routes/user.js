8a7797269bdbb9c05238f5ca493b9f60517aed5a
Harikrishnan-TM <Techdatamaxonline@gmail.com>
2020-11-27 12:18:54 -0800

    now we are using ajax

--- routes/user.js
+++ routes/user.js
@@ -15,14 +15,19 @@ const verifyLogin=(req,res,next)=>{
 }
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
   let user=req.session.user
 
   console.log(user);
+  let cartCount=null
+  if(req.session.user){
+    cartCount=await userHelpers.getCartCount(req.session.user._id) 
+  }
+ 
 
 
   productHelpers.getAllProducts().then((products)=>{
-    res.render('user/view-products',{products,user})
+    res.render('user/view-products',{products,user,cartCount})
   })
 });
 
@@ -69,12 +74,13 @@ router.get('/cart',verifyLogin,async(req,res)=>{
   let products=await userHelpers.getCartProducts(req.session.user._id)
   console.log(products);
 
-  res.render('user/cart',{products})
+  res.render('user/cart',{products,user:req.session.user})
 })
 
-router.get('/add-to-cart/:id',verifyLogin,(req,res)=>{
+router.get('/add-to-cart/:id',(req,res)=>{
+  console.log("api call");
   userHelpers.addToCart(req.params.id,req.session.user._id).then(()=>{
-    res.redirect('/')
+    res.json({status:true})
   })
 
 })
