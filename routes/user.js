38bf1b30368694730c9dbd0a0741d93ea6eccad9
Harikrishnan-TM <Techdatamaxonline@gmail.com>
2020-11-29 09:54:44 -0800

    now users can see the details about their order

--- routes/user.js
+++ routes/user.js
@@ -113,4 +113,8 @@ router.get('/order-success',(req,res)=>{
   res.render('user/order-success',{user:req.session.user})
 })
 
+router.get('/orders',async(req,res)=>{
+  let orders=await userHelpers.getUserOrders(req.session.user._id)
+  res.render('user/orders',{user:req.session.user,orders})
+})
 module.exports = router;
